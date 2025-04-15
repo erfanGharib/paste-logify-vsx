2ca8fd968bf747bddd03059119406bf7bb5760a8
e.gharib <e.gharib@efarda.ir>
2025-04-15 15:38:25 +0330

    fix: do clean up while pasting

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const CONSOLE_GROUP = "console.group(\"\");";
+export const CONSOLE_GROUP_END = "console.groupEnd();";
--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { CONSOLE_GROUP, CONSOLE_GROUP_END } from './constants';
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand('extension.pasteLogify', async () => {
@@ -12,18 +13,31 @@ export function activate(context: vscode.ExtensionContext) {
       .map(v => v.trim())
       .filter(v => v.length > 0);
 
+    const currentLine = editor.document.lineAt(editor.selection.start.line);
+    const indentMatch = currentLine.text.match(/^(\s*)/);
+    const indent = indentMatch ? indentMatch[1] : "";
+    
     const logLines = [
-      `console.group();`,
+      CONSOLE_GROUP,
       ...vars.map(v => `console.log("${v}", ${v});`),
-      `console.groupEnd();`
+      CONSOLE_GROUP_END,
     ];
 
-    const finalText = logLines.join('\n');
+    const finalText = logLines.map(line => indent + line).join('\n');
 
     editor.edit(editBuilder => {
-      const selection = editor.selection;
-      editBuilder.replace(selection, finalText);
+      const startLine = editor.selection.start.line;
+      const lineRange = editor.document.lineAt(startLine).range;
+    
+      editBuilder.replace(lineRange, finalText);
     });
+    
+    // Move the cursor to the line between double quotes in: console.group("")
+    const insertLine = editor.selection.start.line;
+    const currentColumn = indent.length + (CONSOLE_GROUP.length - 3);
+    const newPosition = new vscode.Position(insertLine, currentColumn);
+    editor.selection = new vscode.Selection(newPosition, newPosition);
+    editor.revealRange(new vscode.Range(newPosition, newPosition));
   });
 
   context.subscriptions.push(disposable);
